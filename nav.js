bf9b825173a48a98ab040e95e4c818af3d214b51
Sandraeng <sandra.m.b.eng@hotmail.com>
2022-01-25 22:33:10 +0100

    navbar update

---
+++ nav.js
@@ -0,0 +1,22 @@
+    var html = 
+    '<nav class="navmenu">\
+    <div class="flexcontainer">\
+        <div class="logo">\
+        <a href="Index.html">Home</a>\
+        </div>\
+        <div class="burger">\
+            <div class="line1"></div>\
+            <div class="line2"></div>\
+            <div class="line3"></div>\
+        </div>\
+        <div class="menuoptions">\
+            <div><a href="Portfolio.html">Portfolio</a></div>\
+            <div><a href="Contact.html">Contact</a></div>\
+        </div>\
+    </div>\
+</nav>';
+    
+document.getElementById('navmenu').innerHTML = html
+
+
+
