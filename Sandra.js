b3b1c1c698791093db5b846bb6000831f372351b
Sandraeng <sandra.m.b.eng@hotmail.com>
2022-02-08 10:28:27 +0100

    spring cleaning

--- Sandra.js
+++ Sandra.js
@@ -1,3 +1,31 @@
+var html = 
+    '<header class="topmenu">\
+    <div class="navflexcontainer">\
+    <div class="homelogo">\
+    <a href="Index.html">Hem</a>\
+    </div>\
+    <nav class="navmenu">\
+        <div class="burger">\
+            <div class="line1"></div>\
+            <div class="line2"></div>\
+            <div class="line3"></div>\
+        </div>\
+        <div class="menuoptions">\
+        <ul class="links">\
+            <li><a href="Portfolio.html">Portfolio</a></li>\
+            <li class="menuDrop"><a href="#">Anställda<i class="bx bx-chevron-down"></i></a>\
+                <ul class="subMenu">\
+                    <li><a href="#">Jonas</a></li>\
+                    <li><a href="Sandra.html">Sandra</a></li>\
+                </ul>\
+            </li>\
+            <li><a href="Contact.html">Kontakt</a></li>\
+        </ul>\
+        </div>\
+    </div>\
+    </nav>';
+document.getElementById('navmenu').innerHTML = html
+
 const navDrop = () => {
     const burger = document.querySelector('.burger');
     const menuOptions = document.querySelector('.menuoptions')
